b60e3c486ae1704cca6fc91a15da230550a87c9e
Yiwen Shi <shiyiwenbupt@gmail.com>
2015-06-25 15:42:43 -0500

    initial

---
+++ TimeSheet/Controllers/EmployeeController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TimeSheet.Models;
+
+namespace TimeSheet.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private TimeSheetContext db = new TimeSheetContext();
+
+        //
+        // GET: /Employee/
+
+        public ActionResult Index()
+        {
+            return View(db.tblEmployees.ToList());
+        }
+
+        //
+        // GET: /Employee/Details/5
+
+        public ActionResult Details(Guid id)
+        {
+            tblEmployee tblemployee = db.tblEmployees.Find(id);
+            if (tblemployee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblemployee);
+        }
+
+        //
+        // GET: /Employee/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Employee/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(tblEmployee tblemployee)
+        {
+            if (ModelState.IsValid)
+            {
+                tblemployee.EmpID = Guid.NewGuid();
+                tblemployee.CreateTime = System.DateTime.Now;
+                tblemployee.LatestModifiedTime= System.DateTime.Now;
+                tblemployee.del = false;
+                db.tblEmployees.Add(tblemployee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tblemployee);
+        }
+
+        //
+        // GET: /Employee/Edit/5
+
+        public ActionResult Edit(Guid id)
+        {
+            tblEmployee tblemployee = db.tblEmployees.Find(id);
+            if (tblemployee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblemployee);
+        }
+
+        //
+        // POST: /Employee/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(tblEmployee tblemployee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblemployee).State = EntityState.Modified;
+                tblemployee.LatestModifiedTime = System.DateTime.Now;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tblemployee);
+        }
+
+        //
+        // GET: /Employee/Delete/5
+
+        public ActionResult Delete(Guid id)
+        {
+            tblEmployee tblemployee = db.tblEmployees.Find(id);
+            if (tblemployee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblemployee);
+        }
+
+        //
+        // POST: /Employee/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            tblEmployee tblemployee = db.tblEmployees.Find(id);
+            tblemployee.LatestModifiedTime = System.DateTime.Now;
+            tblemployee.del = true;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TimeSheet/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TimeSheet.Models
+{
+    [MetadataType(typeof(EmployeeMetaData))]
+    public partial class tblEmployee
+    {
+
+        public class EmployeeMetaData
+        {
+            [Required]
+            [DisplayName("First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [DisplayName("Last Name")]
+            public string LastName { get; set; }
+
+            [Required]
+            public string Email { get; set; }
+        }
+    }
+}
