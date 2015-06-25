238930a339a1733f11d15713c3f616a3a7a06e54
Yiwen Shi <shiyiwenbupt@gmail.com>
2015-06-25 17:03:52 -0500

    update

---
+++ TimeSheet/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TimeSheet.Models;
+
+namespace TimeSheet.Models
+{
+    public class HomeController : Controller
+    {
+        private TimeSheetContext db = new TimeSheetContext();
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var tblemployees = db.tblEmployees.Where(emp => emp.del == false);
+            return View(tblemployees.ToList());
+        }
+
+
+        public ActionResult CheckIn(Guid id)
+        {
+            try
+            {
+                tblTimeSheet timesheet = new tblTimeSheet();
+                timesheet.ID = Guid.NewGuid();
+                timesheet.EmpID = id;
+                timesheet.InTime = System.DateTime.Now;
+                db.tblTimeSheets.Add(timesheet);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException);
+                return View();
+            }
+        }
+
+    }
+}
---
+++ TimeSheet/Controllers/TimeSheetController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TimeSheet.Models;
+
+namespace TimeSheet.Controllers
+{
+    public class TimeSheetController : Controller
+    {
+        private TimeSheetContext db = new TimeSheetContext();
+
+        //
+        // GET: /TimeSheet/
+
+        public ActionResult Index()
+        {
+            var tbltimesheets = db.tblTimeSheets.Include(t => t.tblEmployee);
+            return View(tbltimesheets.ToList());
+        }
+
+        //
+        // GET: /TimeSheet/Details/5
+
+        public ActionResult Details(Guid id)
+        {
+            tblTimeSheet tbltimesheet = db.tblTimeSheets.Find(id);
+            if (tbltimesheet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbltimesheet);
+        }
+
+        //
+        // GET: /TimeSheet/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.EmpID = new SelectList(db.tblEmployees, "EmpID", "FirstName");
+            return View();
+        }
+
+        //
+        // POST: /TimeSheet/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(tblTimeSheet tbltimesheet)
+        {
+            if (ModelState.IsValid)
+            {
+                tbltimesheet.ID = Guid.NewGuid();
+                db.tblTimeSheets.Add(tbltimesheet);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.EmpID = new SelectList(db.tblEmployees, "EmpID", "FirstName", tbltimesheet.EmpID);
+            return View(tbltimesheet);
+        }
+
+        //
+        // GET: /TimeSheet/Edit/5
+
+        public ActionResult Edit(Guid id)
+        {
+            tblTimeSheet tbltimesheet = db.tblTimeSheets.Find(id);
+            if (tbltimesheet == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.EmpID = new SelectList(db.tblEmployees, "EmpID", "FirstName", tbltimesheet.EmpID);
+            return View(tbltimesheet);
+        }
+
+        //
+        // POST: /TimeSheet/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(tblTimeSheet tbltimesheet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbltimesheet).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.EmpID = new SelectList(db.tblEmployees, "EmpID", "FirstName", tbltimesheet.EmpID);
+            return View(tbltimesheet);
+        }
+
+        //
+        // GET: /TimeSheet/Delete/5
+
+        public ActionResult Delete(Guid id)
+        {
+            tblTimeSheet tbltimesheet = db.tblTimeSheets.Find(id);
+            if (tbltimesheet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbltimesheet);
+        }
+
+        //
+        // POST: /TimeSheet/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            tblTimeSheet tbltimesheet = db.tblTimeSheets.Find(id);
+            db.tblTimeSheets.Remove(tbltimesheet);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
